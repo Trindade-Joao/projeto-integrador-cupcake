d525fb69b88b224f19c7938143ca6a99dc7d89c1
Trindade-Joao <joao.a.trindade.jr@gmail.com>
2025-11-16 15:56:13 -0300

    #0001 - commit inicial

---
+++ src/main/java/com/cupcakeshop/projeto/ProjetoIntegradorCupcakeApplication.java
@@ -0,0 +1,13 @@
+package com.cupcakeshop.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegradorCupcakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegradorCupcakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cupcakeshop/projeto/ProjetoIntegradorCupcakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cupcakeshop.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIntegradorCupcakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
